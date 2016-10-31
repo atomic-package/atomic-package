d7d7ef7d99e1f275f858701e4038f67a77de2c15
webcyou <webcyou@gmail.com>
2016-10-31 23:28:55 +0900

    add typings files

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,6 @@
+/// <reference path="globals/empower/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/power-assert-formatter/index.d.ts" />
+/// <reference path="globals/power-assert/index.d.ts" />
+/// <reference path="globals/selenium-webdriver/index.d.ts" />
